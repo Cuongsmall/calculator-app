fd644b8134e52777b7fa9a2bcaf3fed7858975ec
Cuongsmall <cuongnmps38652@gmai.com>
2025-04-07 22:51:11 +0700

    commit

---
+++ src/test/java/lab/io/example/CalculatorTest.java
@@ -0,0 +1,35 @@
+package lab.io.example;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(calc.add(2, 3), 5);
+    }
+
+    @Test
+    public void testSubtract() {
+        Assert.assertEquals(calc.subtract(5, 2), 3);
+    }
+
+    @Test
+    public void testMultiply() {
+        Assert.assertEquals(calc.multiply(2, 3), 6);
+    }
+
+    @Test
+    public void testDivide() {
+        Assert.assertEquals(calc.divide(6, 2), 3);
+    }
+
+    @Test(expectedExceptions = ArithmeticException.class)
+    public void testDivideByZero() {
+        calc.divide(5, 0);
+    }
+}
